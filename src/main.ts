ea393dfadb990590c84e44fc8ba311a74a02da72
Pedro <pbsilvab@gmail.com>
2020-09-06 22:21:52 -0300

    socker

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { config as loadEnv } from "dotenv";
 loadEnv();
 import  * as express  from 'express';
 import  { createServer }  from 'http';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import  * as cors  from 'cors';
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./typeDefs";
@@ -16,26 +16,26 @@ const Start = async () => {
 
   const corsOptions = {
     origin: "*",
-    credentials: true // <-- REQUIRED backend setting
+    credentials: false // <-- REQUIRED backend setting
   };
 
   const apolloServer = new ApolloServer({
-    introspection: true,
     resolvers,
     typeDefs,
     context: ({ req , res }) => ({ req, res})
   });
 
-  apolloServer.applyMiddleware({app, cors: true});
+  apolloServer.applyMiddleware({app, cors: false});
 
-  app.use(cors(corsOptions));
-  app.options('*', cors());
+  // app.use(cors(corsOptions));
+ // app.options('*', cors());
 
   const server = createServer(app);
 
   const io = socketIo(server);
 
   io.on('connection', socket => {
+    console.log('someone connected');
       socket.on('join-room', (roomId, userId) => {
         console.log(roomId, userId);
         socket.join(roomId);
